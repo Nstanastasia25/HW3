a8484050a05e29bb5fef8f213a259180f0125fac
Anastasia <nstanastasia25@yandex.ru>
2022-04-26 19:30:52 +0300

    lesson1

---
+++ lesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string text="-Я думаю, - сказал князь,улыбаясь, - что, "
++"ежеи бы вас послали вместо нашего милого Винценгероде,";
+string Replace(string text, char oldValue, char newValue);
+{
+    string result = string.Empty;
+    
+    int lenght = text.Length;
+    for (int i = 0; i<lenght; i++)
+    {
+        if (text [i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+return result;
+}
+string NewText = Replace(Text,' ' ,'|');
+System.Console.WriteLine(NewText);
