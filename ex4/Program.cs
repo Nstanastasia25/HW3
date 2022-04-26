0d1e6d011045c98c74dc723d08961363b5c8779a
Anastasia <nstanastasia25@yandex.ru>
2022-04-26 23:40:20 +0300

    добавила упражнение 4, изменила 2 и 3

---
+++ ex4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+double x1 = 3;
+double y1 = 6;
+double z1 = 8;
+
+double x2 = 2;
+double y2 = 1;
+double z2 = -7;
+
+
+double i = Convert.ToInt32(Math.Pow((x2 - x1), 2));
+double d = Convert.ToInt32(Math.Pow((y2 - y1), 2));
+double f = Convert.ToInt32(Math.Pow((z2 - z1), 2));
+double c = Convert.ToInt32(Math.Pow((i + d + f), 0.5));
+Console.WriteLine(c);
--- ex4/obj/Debug/net6.0/ex4.AssemblyInfo.cs
+++ ex4/obj/Debug/net6.0/ex4.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("lesson2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ex4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("lesson2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("lesson2")]
+[assembly: System.Reflection.AssemblyProductAttribute("ex4")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ex4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
